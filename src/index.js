7be8368403b164a99263cd0b1e0cb9dfc72ad6b4
Pavel <naxa@nextmail.ru>
2021-03-09 23:33:35 +0100

    feat: implement max, min, and avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+    array = array || [];
+    return array.reduce(
+        (mini, e) => Math.min(e, mini),
+        array[0] || 0,
+    );
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    array = array || [];
+    return array.reduce(
+        (maxi, e) => Math.max(e, maxi),
+        array[0] || 0,
+    );
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    array = array || [];
+    const sum = array.reduce((sum, e) => sum + e, 0);
+    return sum / (array.length || 1);
 }
