fbca551571936efae69983598111338ef5eed411
Pavel <naxa@nextmail.ru>
2021-03-11 14:36:07 +0100

    refactor: set default parameter value (ES6)

--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
-exports.min = function min(array) {
-    array = array || [];
+exports.min = function min(array = []) {
     return Math.min(0, ...array);
 }
 
-exports.max = function max(array) {
-    array = array || [];
+exports.max = function max(array = []) {
     return Math.max(0, ...array);
 }
 
-exports.avg = function avg(array) {
-    array = array || [];
+exports.avg = function avg(array = []) {
     const sum = array.reduce((sum, e) => sum + e, 0);
     return sum / (array.length || 1);
 }
